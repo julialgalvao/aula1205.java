3207f5ce60462ce356e178ac6f9689520e13290d
julialgalvao <julialgalvao777@gmail.com>
2023-05-12 10:34:09 -0300

    comeco da aula

---
+++ site1/script.js
@@ -0,0 +1,14 @@
+//RETORNANDO A DATA
+let horas = new Date()
+document.write(horas.getHours());
+document.write("<br><br>")
+//RETORNANDO OS MINUTOS
+let minutos = new Date()
+document.write(minutos.getMinutes());
+document.write("<br><br>")
+
+//RETORNANDO SEGUNDOS
+let segundos = new Date()
+document.write(segundos.getSeconds());
+document.write("<br><br>")
+
