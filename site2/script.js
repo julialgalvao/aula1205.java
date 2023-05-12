999275dcbea20c32724b4b59103db8f93eeb213b
julialgalvao <julialgalvao777@gmail.com>
2023-05-12 11:13:05 -0300

    aula...

--- site2/script.js
+++ site2/script.js
@@ -1,17 +1,3 @@
-//RETORNANDO A DATA
-let horas = new Date()
-document.write(horas.getHours());
-document.write("<br><br>")
-
-//RETORNANDO OS MINUTOS
-let minutos = new Date()
-document.write(minutos.getMinutes());
-document.write("<br><br>")
-
-//RETORNANDO SEGUNDOS
-let segundos = new Date()
-document.write(segundos.getSeconds());
-document.write("<br><br>")
 
 //criando a variavel
 let tmp
